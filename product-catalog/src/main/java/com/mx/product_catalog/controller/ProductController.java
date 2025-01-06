b09a312e3ea207651c6c6e3a2408b7133700783b
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-01-06 19:36:40 -0300

    adding class controller

---
+++ product-catalog/src/main/java/com/mx/product_catalog/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.mx.product_catalog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mx.product_catalog.model.Product;
+import com.mx.product_catalog.service.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+	@Autowired
+	private ProductService service;
+	
+	@PostMapping
+	public Product create(@RequestBody Product product) {
+		
+		return service.create(product);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public Product findById(@PathVariable Long id) throws Exception {
+		
+		return service.findById(id);
+	}
+}
