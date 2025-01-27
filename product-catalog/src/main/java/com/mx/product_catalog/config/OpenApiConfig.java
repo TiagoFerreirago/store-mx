ccefac29682c28528609c35e9226c9d8a500c1c7
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2025-01-27 11:19:47 -0300

    Product-Catalog:adding documentation with swagger UI

--- product-catalog/src/main/java/com/mx/product_catalog/config/OpenApiConfig.java
+++ product-catalog/src/main/java/com/mx/product_catalog/config/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.mx.shopping_cart.config;
+package com.mx.product_catalog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +7,10 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
-
 @Configuration
 public class OpenApiConfig {
 
+	
 	@Bean
 	public OpenAPI customOpenApi() {
 		
@@ -18,7 +18,7 @@ public class OpenApiConfig {
 				.info(new Info()
 						.title("REST API's with Java 17, Spring Boot 3.4.1 and Redis")
 						.version("v1")
-						.description("API for shopping cart management, allowing the addition, removal and search of items")
+						.description("API for product management, allowing you to add new products to the system and search for detailed information on registered products.")
 						.termsOfService("https://www.mx.com/shopping")
 						.license(new License()
 								.name("apache 2.0")
